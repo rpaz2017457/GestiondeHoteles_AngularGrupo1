d8914c8ca8bc876894b4bee07adb010337f67b43
Ricardo Benjamín Paz González <89035320+rpaz2017457@users.noreply.github.com>
2022-07-13 06:21:17 -0600

    final changes admon

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/gestiondehoteles'),
+      dir: require('path').join(__dirname, './coverage/administracion-hoteles'),
       subdir: '.',
       reporters: [
         { type: 'html' },
